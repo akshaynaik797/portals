4bb88fbbbb343305788068331b26e89d61bff0fc
akshaynaik797 <akshaynaik797@gmail.com>
2020-10-14 14:55:26 +0530

    0.0.1 wrote 80% selenium code for mediassist discharge created db

---
+++ settings.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+webdriver_path = '/home/akshay/Downloads/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(webdriver_path, options=chrome_options)
+pass
