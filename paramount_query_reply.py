ea2355ed6765919a84a214e7c593b716ba7becd9
akshaynaik797 <akshaynaik797@gmail.com>
2020-10-28 13:23:46 +0530

    0.0.10 done paramount_query_reply.py

--- paramount_query_reply.py
+++ paramount_query_reply.py
@@ -8,17 +8,17 @@ from gui_functions import capcha_popup
 website = 'https://provider.paramounttpa.com/Login.aspx'
 username, password = 'NEWBALHOSP', 'PPG110092'
 
-ccn = '4778769'
+ccn = '4781468'
 doa, dod = '27/27/2020', '28/10/2020'
 amount = '999'
-remarks = 'discharge'
+remarks = 'query reply'
 files = ['attach/1603516999_101676374_3943.pdf', 'attach/8_VNUPEHPG.869375402960_duly_signed_doc_1603511124.pdf']
 
 username_input = '/html/body/form/div[3]/div[2]/div[1]/div/div/div/div[2]/fieldset/input[1]'
 password_input = '/html/body/form/div[3]/div[2]/div[1]/div/div/div/div[2]/fieldset/input[2]'
 login_btn = '/html/body/form/div[3]/div[2]/div[1]/div/div/div/div[2]/fieldset/input[3]'
 e_cashless_btn = '/html/body/form/section/div/section/header/div[2]/li[2]'
-enhancement_request_btn = '/html/body/form/section/div/section/header/div[2]/li[2]/ul/li[2]/a/span'
+enhancement_request_btn = '/html/body/form/section/div/section/header/div[2]/li[2]/ul/li[5]/a/span'
 ccn_input = '/html/body/form/section/div/section/div/section/section/div/section/div/div[1]/div/div/input'
 ccn_search_btn = '/html/body/form/section/div/section/div/section/section/div/section/div/div[2]/div/div/input'
 request_for_option = '/html/body/form/section/div/section/div/section/section/div[2]/section/div/table/tbody/tr/td[1]/select/option[2]'
@@ -40,18 +40,9 @@ click('xpath', e_cashless_btn)
 click('xpath', enhancement_request_btn)
 send_keys('xpath', ccn_input, ccn)
 click('xpath', ccn_search_btn)
-click('xpath', request_for_option)
-click('xpath', enhancement_request2_btn)
-send_keys('xpath', doa_input, doa)
-send_keys('xpath', dod_input, dod)
-click('xpath', probalble_loss_btn)
-click('xpath', submit_btn)
-send_keys('xpath', total_amt_input, amount)
 send_keys('xpath', remark_input, remarks)
-# a = WebDriverWait(driver, 25).until(EC.visibility_of_element_located((By.XPATH, drag_drop_input)))
 for i in files:
     send_keys('xpath', drag_drop_input, os.path.abspath(i))
-    # drag_and_drop_file(a, os.path.abspath(i))
 click('xpath', add_files_btn)
 sleep(1000)
 driver.close()
